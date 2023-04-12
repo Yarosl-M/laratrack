830ba4ff2c9aa0a37a8f4b70cc12d93460250065
Yarosl-M <93162306+Yarosl-M@users.noreply.github.com>
2023-04-12 12:46:41 +0300

    Define Ticket relationships

--- app/Models/Ticket.php
+++ app/Models/Ticket.php
@@ -5,6 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Concerns\HasUlids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 /* A tech support ticket. */
 class Ticket extends Model
@@ -30,5 +33,27 @@ class Ticket extends Model
 
     public function assignedTo($query, User $user) {
         return $query->where('assigned_to', $user->id);
+    }
+
+    public function thread_entries(): HasMany {
+        return $this->hasMany(ThreadEntry::class, 'ticket_id');
+    }
+    public function messages(): HasMany {
+        return $this->hasMany(Message::class, 'ticket_id');
+    }
+    public function thread_actions(): HasMany {
+        return $this->hasMany(ThreadAction::class, 'ticket_id');
+    }
+    public function client(): BelongsTo {
+        return $this->belongsTo(User::class, 'client_id');
+    }
+    public function assignee(): BelongsTo {
+        return $this->belongsTo(User::class, 'assigned_to');
+    }
+    public function priority(): BelongsTo {
+        return $this->belongsTo(Priority::class, 'priority_id');
+    }
+    public function tags(): BelongsToMany {
+        return $this->belongsToMany(Tag::class);
     }
 }
