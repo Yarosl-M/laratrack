ede4b99a03ee352194e4c3351b8ff00462be3d60
Yarosl-M <93162306+Yarosl-M@users.noreply.github.com>
2023-04-20 21:40:56 +0300

    Add FileController

---
+++ app/Http/Controllers/FileController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+// encapsulates working with files (e. g. to set up access permissions)
+class FileController extends Controller
+{
+}
