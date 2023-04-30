4876442fdfc701d050d71bdfaa2997983ef4e78c
Yarosl-M <93162306+Yarosl-M@users.noreply.github.com>
2023-04-30 22:18:15 +0300

    Create TagPolicy

---
+++ app/Policies/TagPolicy.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Policies;
+
+use App\Enums\UserType;
+use App\Models\Tag;
+use App\Models\User;
+use Illuminate\Auth\Access\Response;
+
+class TagPolicy {
+    public function viewAny(User $user): bool {
+        return $user->type === UserType::Operator->value || $user->type === UserType::Admin->value;
+    }
+    public function view(User $user, Tag $tag): bool {
+        return $user->type === UserType::Operator->value || $user->type === UserType::Admin->value;
+    }
+    public function create(User $user): bool {
+        return $user->hasPermission('edit_tags');
+    }
+    public function update(User $user, Tag $tag): bool {
+        return $user->hasPermission('edit_tags');
+    }
+    public function delete(User $user, Tag $tag): bool {
+        return $user->hasPermission('edit_tags');
+    }
+}
