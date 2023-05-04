2574d3b95bef88d209417e2b5769c5072e3cf6d7
Yarosl-M <93162306+Yarosl-M@users.noreply.github.com>
2023-05-04 15:00:38 +0300

    Create CreateTicketRequest

---
+++ app/Http/Requests/CreateTicketRequest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Models\Ticket;
+use Illuminate\Foundation\Http\FormRequest;
+
+class CreateTicketRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return $this->user()->can('create', Ticket::class);
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\Rule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'subject' => 'bail|required|max:100',
+            'content' => 'bail|required|max:1000'
+        ];
+    }
+}
