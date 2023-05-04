b4f6c4ecaa818e7c7190f72c64d2b87841d628d5
Yarosl-M <93162306+Yarosl-M@users.noreply.github.com>
2023-05-05 01:10:50 +0300

    Implement logging in

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,9 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\AuthenticateRequest;
+use App\Services\UserService;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    public function __construct(private UserService $userService) {}
+
+    public function create() {
+        return view('users.register');
+    }
+
+    public function store(Request $request) {
+        // TODO: figure out where and how to validate
+        $formFields = $request->only(['email', 'username', 'password', 'password_confirmation', 'name']);
+    }
+
+    public function authenticate(AuthenticateRequest $request) {
+        $attr = $request->safe()->only('email', 'password');
+        if ($this->userService->authenticate($attr)) {
+            return 'Successfully signed in';
+        }
+        else return 'Fail';
+    }
 }
