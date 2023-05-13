3d2f9c3bc31b705474d419a64bf4f4ca8727dec7
Yarosl-M <93162306+Yarosl-M@users.noreply.github.com>
2023-05-13 17:23:38 +0300

    Add authentication/guest middleware

--- routes/web.php
+++ routes/web.php
@@ -17,45 +17,49 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+// for now
+Route::get('/', [TicketController::class, 'index']);
 
 Route::get('/test', function () {
     return view('users.login');
 });
 
-// for now
-Route::get('/', [TicketController::class, 'index']);
+Route::middleware('guest')->group(function() {
+    // получить форму входа
+    Route::get('/login', [UserController::class, 'login'])->name('login');
+    // получить форму регистрации
+    Route::get('/register', [UserController::class, 'create']);
+    // зарегистрироваться
+    Route::post('/users', [UserController::class, 'store']);
+    // войти в систему
+    Route::post('/users/authenticate', [UserController::class, 'authenticate']);
+});
 
-Route::get('/tickets', [TicketController::class, 'index']);
+// только для авторизованных пользователей
+Route::middleware('auth')->group(function() {
+    Route::get('/tickets', [TicketController::class, 'index']);
 
-Route::get('/tickets/archive', [TicketController::class, 'archive']);
+    Route::get('/tickets/archive', [TicketController::class, 'archive']);
 
-// форма создания тикета
-Route::get('/tickets/create', [TicketController::class, 'create']);
-// страница тикета
-Route::get('/tickets/{ticket}', [TicketController::class, 'show']);
+    // форма создания тикета
+    Route::get('/tickets/create', [TicketController::class, 'create']);
+    // страница тикета
+    Route::get('/tickets/{ticket}', [TicketController::class, 'show']);
 
-// открыть параметры тикета
-Route::get('/tickets/{ticket}/settings', [TicketController::class, 'settings']);
-// сохранить параметры тикета
-Route::post('/tickets/{ticket}/settings', [TicketController::class, 'update']);
+    // открыть параметры тикета
+    Route::get('/tickets/{ticket}/settings', [TicketController::class, 'settings']);
+    // сохранить параметры тикета
+    Route::post('/tickets/{ticket}/settings', [TicketController::class, 'update']);
 
-Route::post('/tickets/{ticket}/comment', [TicketController::class, 'comment']);
+    Route::post('/tickets/{ticket}/comment', [TicketController::class, 'comment']);
 
-Route::post('/tickets', [TicketController::class, 'store']);
+    Route::post('/tickets', [TicketController::class, 'store']);
 
-// получить форму входа
-Route::get('/login', [UserController::class, 'login']);
-// получить форму регистрации
-Route::get('/register', [UserController::class, 'create']);
+    Route::post('/logout', [UserController::class, 'logout']);
 
-Route::post('/logout', [UserController::class, 'logout']);
-// зарегистрироваться
-Route::post('/users', [UserController::class, 'store']);
-// войти в систему
-Route::post('/users/authenticate', [UserController::class, 'authenticate']);
+    Route::get('/account', [UserController::class, 'edit']);
+    Route::get('/account/change-password', [UserController::class, 'change_password']);
 
-Route::get('/account', [UserController::class, 'edit']);
-Route::get('/account/change-password', [UserController::class, 'change_password']);
-
-Route::post('/account/update-profile-picture', [UserController::class, 'update_pfp']);
-Route::post('/users/change-password', [UserController::class, 'update_password']);
+    Route::post('/account/update-profile-picture', [UserController::class, 'update_pfp']);
+    Route::post('/users/change-password', [UserController::class, 'update_password']);
+});
