f4041328121c15a5a1f864a55a82acae83ac0ad0
Yarosl-M <93162306+Yarosl-M@users.noreply.github.com>
2023-05-15 20:28:34 +0300

    Remove unused Blade files

--- resources/views/partials/_search.blade.php
+++
@@ -1,6 +0,0 @@
-<form class="search" action="">
-    <div>
-        <input type="text" name="search" size="50" value="{{old('search')}}" placeholder="Введите поисковый запрос…">
-        <button type="submit">Поиск</button>
-    </div>
-</form>
