75e68c0e21ebf830a0b05474c76efa8844e0fa06
Yarosl-M <93162306+Yarosl-M@users.noreply.github.com>
2023-05-16 22:51:26 +0300

    Add errors display to register view

--- resources/views/users/register.blade.php
+++ resources/views/users/register.blade.php
@@ -10,14 +10,29 @@
         {{-- also don't forget error messages --}}
         @csrf
         <label for="email" class="required">E-mail</label>
+        @error('email')
+            <p class="error">{{$message}}</p>
+        @enderror
         <input type="text" name="email" value="{{old('email')}}"/>
         <label for="username" class="required">Имя пользователя (логин)</label>
+        @error('username')
+            <p class="error">{{$message}}</p>
+        @enderror
         <input type="text" name="username" value="{{old('username')}}"/>
         <label for="name">Ваше имя (будет отображаться другим в системе)</label>
+        @error('name')
+            <p class="error">{{$message}}</p>
+        @enderror
         <input type="text" name="name" value="{{old('name')}}"/>
         <label for="password" class="required">Пароль</label>
+        @error('password')
+            <p class="error">{{$message}}</p>
+        @enderror
         <input type="password" name="password"/>
         <label for="password_confirmation" class="required">Повторите пароль</label>
+        @error('password_confirmation')
+            <p class="error">{{$message}}</p>
+        @enderror
         <input type="password" name="password_confirmation"/>
         <div>
             <button type="submit">Зарегистрироваться</button>
