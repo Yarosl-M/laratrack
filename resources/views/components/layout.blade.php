d8f9f0892eb2180c0c435530815698affd2da97c
Yarosl-M <93162306+Yarosl-M@users.noreply.github.com>
2023-05-18 04:21:37 +0300

    Fix the jQuery script

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -10,8 +10,7 @@
         </title>
         <link rel="stylesheet" href="{{asset("css/style.css")}}">
         <link rel="stylesheet" href="{{asset("css/style_layout.css")}}">
-        <script src="https://code.jquery.com/jquery-3.7.0.min.js" integrity="sha256-a9jBBRygX1Bh5lt8GZjXDzyOB+bWve9EiO7tROUtj/E=" crossorigin="anonymous"></script>
-        @isset($stylesheets)
+        <script src="https://code.jquery.com/jquery-3.7.0.min.js" integrity="sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g=" crossorigin="anonymous"></script>        @isset($stylesheets)
             @foreach ($stylesheets as $sheet)
                 <link rel="stylesheet" href="{{asset("css/" . $sheet . ".css")}}">
             @endforeach
