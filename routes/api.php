081cf34e4a7fa1d8077318c24d43d779972b5071
Yarosl-M <93162306+Yarosl-M@users.noreply.github.com>
2023-05-18 03:30:20 +0300

    Add missing alias

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TagApiController;
 use App\Http\Controllers\UserApiController;
 use App\Http\Controllers\TicketApiController;
 
