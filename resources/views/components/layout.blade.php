edb49f03123c7990157c844bc6b0e6865cb5e8a6
Yarosl-M <93162306+Yarosl-M@users.noreply.github.com>
2023-05-18 03:31:44 +0300

    Update jQuery to minified version

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -10,7 +10,7 @@
         </title>
         <link rel="stylesheet" href="{{asset("css/style.css")}}">
         <link rel="stylesheet" href="{{asset("css/style_layout.css")}}">
-        <script src="https://code.jquery.com/jquery-3.6.4.js" integrity="sha256-a9jBBRygX1Bh5lt8GZjXDzyOB+bWve9EiO7tROUtj/E=" crossorigin="anonymous"></script>
+        <script src="https://code.jquery.com/jquery-3.7.0.min.js" integrity="sha256-a9jBBRygX1Bh5lt8GZjXDzyOB+bWve9EiO7tROUtj/E=" crossorigin="anonymous"></script>
         @isset($stylesheets)
             @foreach ($stylesheets as $sheet)
                 <link rel="stylesheet" href="{{asset("css/" . $sheet . ".css")}}">
