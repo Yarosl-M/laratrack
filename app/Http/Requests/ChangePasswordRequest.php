a6c5af4cc48dba20f48b655222793f24c5e75705
Yarosl-M <93162306+Yarosl-M@users.noreply.github.com>
2023-05-21 20:08:25 +0300

    Add validation errors to change password

--- app/Http/Requests/ChangePasswordRequest.php
+++ app/Http/Requests/ChangePasswordRequest.php
@@ -16,12 +16,21 @@ class ChangePasswordRequest extends FormRequest
      *
      * @return array<string, \Illuminate\Contracts\Validation\Rule|array|string>
      */
-    public function rules(): array
-    {
+    public function rules(): array {
         return [
             'current_password' => ['bail', 'required'],
             'password' => ['bail', 'required', 'min:6', 'max:40', 'regex:/[A-Za-z0-9*.!@#$%^&(){}[\]:;<>,.?\/~_+\-=|\\\]*/i',
                 'confirmed'],
+        ];
+    }
+    public function messages(): array {
+        return [
+            'current_password.required' => 'Это обязательное поле',
+            'password.required' => 'Это обязательное поле',
+            'password.min' => 'Пароль не должен быть короче 6 символов',
+            'password.max' => 'Пароль не должен быть длиннее 40 символов',
+            'password.regex' => 'Пароль содержит недопустимые символы',
+            'password.confirmed' => 'Пароль неправильно подтверждён',
         ];
     }
 }
--- resources/views/users/change-password.blade.php
+++ resources/views/users/change-password.blade.php
@@ -9,10 +9,19 @@
             <p class="error">{{$message}}</p>
         @enderror
         <label for="current_password">Текущий пароль</label>
+        @error('current_password')
+            <p class="error">{{$message}}</p>
+        @enderror
         <input type="password" name="current_password"/>
         <label for="password">Новый пароль</label>
+        @error('password')
+            <p class="error">{{$message}}</p>
+        @enderror
         <input type="password" name="password"/>
         <label for="password_confirmation">Повторите пароль</label>
+        @error('password_confirmation')
+            <p class="error">{{$message}}</p>
+        @enderror
         <input type="password" name="password_confirmation"/>
         <div>
             <button type="submit">Изменить пароль</button>
