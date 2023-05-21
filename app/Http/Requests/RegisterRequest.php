2b7f12339d1db87b4a8d1974e4ffdd87c54f6e7c
Yarosl-M <93162306+Yarosl-M@users.noreply.github.com>
2023-05-21 20:36:31 +0300

    Add validation error messages to register

--- app/Http/Requests/RegisterRequest.php
+++ app/Http/Requests/RegisterRequest.php
@@ -18,14 +18,31 @@ class RegisterRequest extends FormRequest
      *
      * @return array<string, \Illuminate\Contracts\Validation\Rule|array|string>
      */
-    public function rules(): array
-    {
+    public function rules(): array {
         return [
             'email' => 'bail|required|email|unique:users',
             'username' => ['bail', 'required', 'regex:/[A-Za-z][A-Za-z0-9_]*/i', 'min:6', 'max:40', 'unique:users'],
             'name' => 'bail|nullable|max:100',
             'password' => ['bail', 'required', 'min:6', 'max:40', /* apparently single quotes make this work??? */ 'regex:/[A-Za-z0-9*.!@#$%^&(){}[\]:;<>,.?\/~_+\-=|\\\]*/i',
                 'confirmed'],
+        ];
+    }
+    public function messages(): array {
+        return [
+            'email.required' => 'Это поле обязательно для заполнения',
+            'email.email' => 'Это поле должно быть корректным адресом e-mail',
+            'email.unique' => 'Такой адрес e-mail уже занят',
+            'username.required' => 'Это поле обязательно для заполнения',
+            'username.regex' => 'Поле должно включать только латинские буквы, цифры и знак подчёркивания',
+            'username.min' => 'Логин должен быть не короче 6 символов',
+            'username.max' => 'Логин должен быть не длиннее 40 символов',
+            'username.unique' => 'Такой логин уже занят',
+            'name.max' => 'Имя должно быть не длиннее 100 символов',
+            'password.required' => 'Это обязательное поле',
+            'password.min' => 'Пароль не должен быть короче 6 символов',
+            'password.max' => 'Пароль не должен быть длиннее 40 символов',
+            'password.regex' => 'Пароль содержит недопустимые символы',
+            'password.confirmed' => 'Пароль неправильно подтверждён',
         ];
     }
 }
